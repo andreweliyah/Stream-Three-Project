ee229d1ba00b5f9ba0c376eacce15ca93ce386ab
Andrew Eliyah <andreweliyah@icloud.com>
2018-08-24 06:50:20 -0700

    Finished adding tinymce to blog

--- BlogReaderPro/BlogReaderPro/urls.py
+++ BlogReaderPro/BlogReaderPro/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     url(r'^api-token-refresh/', refresh_jwt_token),
     url(r'^api-token-verify/', verify_jwt_token),
     url(r'^api-account', include('accounts.api.urls')),
+    url(r'^tinymce/', include('tinymce.urls')),
 ]

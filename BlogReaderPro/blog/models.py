7e250320d17a4ac7ec4e00ba1975e7d64affa88b
Andrew Eliyah <andreweliyah@icloud.com>
2018-08-24 06:43:53 -0700

    Finally commiting blog models
    kept putting off commiting blog models because it kept changing

--- BlogReaderPro/blog/models.py
+++ BlogReaderPro/blog/models.py
@@ -2,5 +2,26 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
+from devenv.settings import AUTH_USER_MODEL as User
+from tinymce.models import HTMLField
 
-# Create your models here.
+class Post(models.Model):
+  author = models.ForeignKey(User,blank=True)
+  title = models.CharField(max_length=200)
+  content = HTMLField()
+  created_date = models.DateTimeField(auto_now_add=True)
+  published_date = models.DateTimeField(blank=True, null=True)
+  views = models.IntegerField(default=0)
+  tag = models.CharField(max_length=30, blank=True, null=True)
+  image = models.ImageField(upload_to="images", blank=True, null=True)
+
+  # def snippet(self):
+  #   return self.content[:50] + '...'
+
+  def publish(self):
+    self.published_date = timezone.now()
+    self.save()
+
+  def __unicode__(self):
+    return self.title

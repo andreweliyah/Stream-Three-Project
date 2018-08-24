1e5e030b2b2708efcd905bc8202e9da7151ebfb6
Andrew Eliyah <andreweliyah@icloud.com>
2018-08-24 06:47:43 -0700

    Added blog admin

--- BlogReaderPro/blog/admin.py
+++ BlogReaderPro/blog/admin.py
@@ -2,5 +2,14 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+class Admin_Post(admin.ModelAdmin):
+  list_display = ['title','author','created_date','published_date','tag','image']
+  list_editable = [
+    'tag',
+    'author',
+    # 'image'
+  ]
+
+admin.site.register(Post, Admin_Post)
